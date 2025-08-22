219c64c5d1025cc216075cc7b0cb621f2c42e4a6
Bhagaskara <bhagaskaraliancer@gmail.com>
2025-08-22 14:44:31 +0700

    Init

---
+++ resources/js/livewire-events.js
@@ -0,0 +1,28 @@
+document.addEventListener('livewire:load', function () {
+    Livewire.on("SwalInfo", (event) => {
+        Swal.fire({
+            icon: event[0],
+            title: event[1],
+            text: event[2],
+        });
+    });
+
+    Livewire.on("SwalConfirm", (event) => {
+        Swal.fire({
+            icon: event[0],
+            title: event[1],
+            text: event[2],
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: event[3],
+            cancelButtonText: event[4],
+        }).then((result) => {
+            if (result.value) {
+                Livewire.dispatch(event[5]);
+            } else {
+                Livewire.dispatch(event[6]);
+            }
+        });
+    });
+});
---
+++ src/LivewireSweetalertServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaraPack\LivewireSweetalert;
+
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+class LivewireSweetalertServiceProvider extends ServiceProvider
+{
+    public function register() {}
+
+    public function boot()
+    {
+        Livewire::provideAssets([
+            'js' => __DIR__ . '/../resources/js/livewire-events.js',
+        ]);
+    }
+}
